fac19310f616cbc9bd6b2ad7f1112e3869501d7b
Vi Ness <kevfol@hotmail.com>
2023-06-11 07:01:43 -1000

    MemoryIO logic and Async methods Rewrote logic for Read/Write methods using MemoryMarshal.Cast. This removes the dependency on MemoryIO.Internals.MarshalType<T>. ReadString now uses ReadOnlySpan to find the null character and avoids string.SubString.
    Added Async versions of all Read/Write methods
    
    Added regions for all types of MemoryIO methods to make it easier to view only one type of method

--- MemoryIO.Pine/PineMemoryIO.cs
+++ MemoryIO.Pine/PineMemoryIO.cs
@@ -1,6 +1,6 @@
-﻿using MemoryIO.Internals;
-using MemoryIO.Pine.Internals;
+﻿using MemoryIO.Pine.Internals;
 using System.Net.Sockets;
+using System.Runtime.InteropServices;
 using System.Text;
 
 namespace MemoryIO.Pine
@@ -36,119 +36,215 @@ namespace MemoryIO.Pine
 
         }
 
-        public T Read<T>(IntPtr address) where T : unmanaged
-        {
-            int dataSize = MarshalType<T>.Size;
-            byte[] buffer = ReadData(address, dataSize);
-            return MarshalType<T>.ByteArrayToObject(buffer);
-        }
-        public async Task<T> ReadAsync<T>(IntPtr address)
-        {
-            int dataSize = MarshalType<T>.Size;
-            byte[] buffer = await ReadDataAsync(address, dataSize);
-            return MarshalType<T>.ByteArrayToObject(buffer);
-        }
+        #region MemoryIO
 
-        public T[] ReadArray<T>(IntPtr address, int arrayLength) where T : unmanaged
-        {
-            int dataSize = MarshalType<T>.Size;
-            byte[] buffer = ReadData(address, dataSize * arrayLength);
-            if (MarshalType<T>.TypeCode == TypeCode.Byte)
-                return (T[])(object)buffer;
-            T[] tArray = new T[arrayLength];
-            for (int i = 0; i < arrayLength; i++)
-                tArray[i] = MarshalType<T>.ByteArrayToObject(buffer[(dataSize * i)..(dataSize * (i + 1))]);
-            return tArray;
-        }
+        #region Read
 
-        public byte[] ReadData(IntPtr address, int dataLength)
-        {
-            int address32bit = (int)address;
-            AnswerMessage answer = tcpQueuer.SendRequestAsync(RequestBuilder.BuildBatchReadMsg(address32bit, dataLength)).GetAwaiter().GetResult();
-            return answer.Argument;
-        }
+        #region ReadData
+        public byte[] ReadData(IntPtr address, int dataLength) => ReadData((int)address, dataLength);
+        public byte[] ReadData(int address, int dataLength) => tcpQueuer.SendRequestAsync(RequestBuilder.BuildBatchReadMsg(address, dataLength)).GetAwaiter().GetResult().Argument;
+        public async Task<byte[]> ReadDataAsync(IntPtr address, int dataLength) => await ReadDataAsync((int)address, dataLength);
+        public async Task<byte[]> ReadDataAsync(int address, int dataLength) => (await tcpQueuer.SendRequestAsync(RequestBuilder.BuildBatchReadMsg(address, dataLength))).Argument;
+        #endregion
 
-        public async Task<byte[]> ReadDataAsync(IntPtr address, int dataLength)
-        {
-            int address32bit = (int)address;
-            AnswerMessage answer = await tcpQueuer.SendRequestAsync(RequestBuilder.BuildBatchReadMsg(address32bit, dataLength));
-            return answer.Argument;
-        }
+        #region Read<T>
+        public T Read<T>(IntPtr address) where T : unmanaged => MemoryMarshal.Cast<byte, T>(ReadData(address, Marshal.SizeOf<T>()))[0];
+        public T Read<T>(int address) where T : unmanaged => MemoryMarshal.Cast<byte, T>(ReadData(address, Marshal.SizeOf<T>()))[0];
+        public async Task<T> ReadAsync<T>(IntPtr address) where T : unmanaged => MemoryMarshal.Cast<byte, T>(await ReadDataAsync(address, Marshal.SizeOf<T>()))[0];
+        public async Task<T> ReadAsync<T>(int address) where T : unmanaged => MemoryMarshal.Cast<byte, T>(await ReadDataAsync(address, Marshal.SizeOf<T>()))[0];
+        #endregion
 
+        #region ReadArray<T>
+        public T[] ReadArray<T>(IntPtr address, int arrayLength) where T : unmanaged => MemoryMarshal.Cast<byte, T>(ReadData(address, Marshal.SizeOf<T>() * arrayLength)).ToArray();
+        public T[] ReadArray<T>(int address, int arrayLength) where T : unmanaged => MemoryMarshal.Cast<byte, T>(ReadData(address, Marshal.SizeOf<T>() * arrayLength)).ToArray();
+        public async Task<T[]> ReadArrayAsync<T>(IntPtr address, int arrayLength) where T : unmanaged => MemoryMarshal.Cast<byte, T>(await ReadDataAsync(address, Marshal.SizeOf<T>() * arrayLength)).ToArray();
+        public async Task<T[]> ReadArrayAsync<T>(int address, int arrayLength) where T : unmanaged => MemoryMarshal.Cast<byte, T>(await ReadDataAsync(address, Marshal.SizeOf<T>() * arrayLength)).ToArray();
+        #endregion
+
+        #region ReadString
         public string ReadString(IntPtr address, Encoding encoding, int maxLength = 512)
         {
-            string encoded = encoding.GetString(ReadData(address, maxLength));
-            int nullCharPos = encoded.IndexOf('\0');
-            return nullCharPos == -1 ? encoded : encoded.Substring(0, nullCharPos);
+            byte[] buffer = ReadData(address, maxLength);
+            ReadOnlySpan<byte> bytes = buffer.AsSpan();
+
+            int nullCharPos = bytes.IndexOf((byte)'\0');
+            if (nullCharPos != -1)
+                bytes = bytes.Slice(0, nullCharPos);
+
+            return encoding.GetString(bytes);
+        }
+        public string ReadString(int address, Encoding encoding, int maxLength = 512)
+        {
+            byte[] buffer = ReadData(address, maxLength);
+            ReadOnlySpan<byte> bytes = buffer.AsSpan();
+
+            int nullCharPos = bytes.IndexOf((byte)'\0');
+            if (nullCharPos != -1)
+                bytes = bytes.Slice(0, nullCharPos);
+
+            return encoding.GetString(bytes);
         }
         public async Task<string> ReadStringAsync(IntPtr address, Encoding encoding, int maxLength = 512)
         {
-            string encoded = encoding.GetString(await ReadDataAsync(address, maxLength));
-            int nullCharPos = encoded.IndexOf('\0');
-            return nullCharPos == -1 ? encoded : encoded.Substring(0, nullCharPos);
-        }
+            byte[] buffer = await ReadDataAsync(address, maxLength);
+            ReadOnlyMemory<byte> memory = buffer;
 
+            int nullCharPos = memory.Span.IndexOf((byte)'\0');
+            if (nullCharPos != -1)
+                memory = memory.Slice(0, nullCharPos);
+
+            return encoding.GetString(memory.Span);
+        }
+        public async Task<string> ReadStringAsync(int address, Encoding encoding, int maxLength = 512)
+        {
+            byte[] buffer = await ReadDataAsync(address, maxLength);
+            ReadOnlyMemory<byte> memory = buffer;
+
+            int nullCharPos = memory.Span.IndexOf((byte)'\0');
+            if (nullCharPos != -1)
+                memory = memory.Slice(0, nullCharPos);
+
+            return encoding.GetString(memory.Span);
+        }
+        #endregion
+
+        #region ReadStringArray
         public string[] ReadStringArray(IntPtr address, Encoding encoding, int maxLength = 512)
         {
             string encoded = encoding.GetString(ReadData(address, maxLength));
             string[] split = encoded.Split('\0');
-            for (int i = 0; i < split.Length; i++)
-                if (split[i] == string.Empty)
-                    return split[0..i];
-            return split;
+            int emptyIndex = Array.IndexOf(split, string.Empty);
+            return emptyIndex >= 0 ? split[..emptyIndex] : split;
         }
-
-        public void Write<T>(IntPtr address, T value) where T : unmanaged
+        public string[] ReadStringArray(int address, Encoding encoding, int maxLength = 512)
         {
-            WriteData(address, MarshalType<T>.ObjectToByteArray(value));
+            string encoded = encoding.GetString(ReadData(address, maxLength));
+            string[] split = encoded.Split('\0');
+            int emptyIndex = Array.IndexOf(split, string.Empty);
+            return emptyIndex >= 0 ? split[..emptyIndex] : split;
         }
-
-        public void WriteArray<T>(IntPtr address, T[] value) where T : unmanaged
+        public async Task<string[]> ReadStringArrayAsync(IntPtr address, Encoding encoding, int maxLength = 512)
         {
-            if (value is byte[] byteArray)
-            {
-                WriteData(address, byteArray);
-                return;
-            }
-            int typeSize = MarshalType<T>.Size;
-            byte[] buffer = new byte[value.Length * typeSize];
-            for (int i = 0; i < value.Length; i++)
-            {
-                Buffer.BlockCopy(MarshalType<T>.ObjectToByteArray(value[i]), 0, buffer, i * typeSize, typeSize);
-            }
-            WriteData(address, buffer);
+            string encoded = encoding.GetString(await ReadDataAsync(address, maxLength));
+            string[] split = encoded.Split('\0');
+            int emptyIndex = Array.IndexOf(split, string.Empty);
+            return emptyIndex >= 0 ? split[..emptyIndex] : split;
         }
-
-        public void WriteData(IntPtr address, byte[] data)
+        public async Task<string[]> ReadStringArrayAsync(int address, Encoding encoding, int maxLength = 512)
         {
-            // If in the future we can all the write methods to return bool, we can check AnswerMessage.ResultCode
-            /* AnswerMessage answer =*/ tcpQueuer.SendRequestAsync(RequestBuilder.BuildBatchWriteMsg((int)address, data)).GetAwaiter().GetResult();
+            string encoded = encoding.GetString(await ReadDataAsync(address, maxLength));
+            string[] split = encoded.Split('\0');
+            int emptyIndex = Array.IndexOf(split, string.Empty);
+            return emptyIndex >= 0 ? split[..emptyIndex] : split;
         }
+        #endregion
 
-        public async Task WriteDataAsync(IntPtr address, byte[] data)
-        {
-           /* AnswerMessage answer =*/ await tcpQueuer.SendRequestAsync(RequestBuilder.BuildBatchWriteMsg((int)address, data));
-        }
+        #endregion
 
-        public void WriteString(IntPtr address, string text, Encoding encoding)
-        {
-            WriteData(address, encoding.GetBytes(text + '\0'));
-        }
+        #region Write
 
+        #region WriteData
+        // tcpQueuer.SendRequestAsync returns an AnswerMessage if in the future we want to add error handling
+        public void WriteData(IntPtr address, byte[] data) => WriteData((int)address, data);
+        public void WriteData(int address, byte[] data) => tcpQueuer.SendRequestAsync(RequestBuilder.BuildBatchWriteMsg(address, data)).GetAwaiter().GetResult();
+        public async Task WriteDataAsync(IntPtr address, byte[] data) => await WriteDataAsync((int)address, data);
+        public async Task WriteDataAsync(int address, byte[] data) => await tcpQueuer.SendRequestAsync(RequestBuilder.BuildBatchWriteMsg(address, data));
+        #endregion
+
+        #region Write<T>
+        public void Write<T>(IntPtr address, T value) where T : unmanaged => WriteData(address, MemoryMarshal.Cast<T, byte>(MemoryMarshal.CreateReadOnlySpan(ref value, 1)).ToArray());
+        public void Write<T>(int address, T value) where T : unmanaged => WriteData(address, MemoryMarshal.Cast<T, byte>(MemoryMarshal.CreateReadOnlySpan(ref value, 1)).ToArray());
+        public async Task WriteAsync<T>(IntPtr address, T value) where T : unmanaged => await WriteDataAsync(address, MemoryMarshal.Cast<T, byte>(MemoryMarshal.CreateReadOnlySpan(ref value, 1)).ToArray());
+        public async Task WriteAsync<T>(int address, T value) where T : unmanaged => await WriteDataAsync(address, MemoryMarshal.Cast<T, byte>(MemoryMarshal.CreateReadOnlySpan(ref value, 1)).ToArray());
+        #endregion
+
+        #region WriteArray<T>
+        public void WriteArray<T>(IntPtr address, T[] value) where T : unmanaged => WriteData(address, MemoryMarshal.Cast<T, byte>(value.AsSpan()).ToArray());
+        public void WriteArray<T>(int address, T[] value) where T : unmanaged => WriteData(address, MemoryMarshal.Cast<T, byte>(value.AsSpan()).ToArray());
+        public async Task WriteArrayAsync<T>(IntPtr address, T[] value) where T : unmanaged => await WriteDataAsync(address, MemoryMarshal.Cast<T, byte>(value.AsSpan()).ToArray());
+        public async Task WriteArrayAsync<T>(int address, T[] value) where T : unmanaged => await WriteDataAsync(address, MemoryMarshal.Cast<T, byte>(value.AsSpan()).ToArray());
+        #endregion
+
+        #region WriteString
+        public void WriteString(IntPtr address, string text, Encoding encoding) => WriteData(address, encoding.GetBytes(text + '\0'));
+        public void WriteString(int address, string text, Encoding encoding) => WriteData(address, encoding.GetBytes(text + '\0'));
+        public async Task WriteStringAsync(IntPtr address, string text, Encoding encoding) => await WriteDataAsync(address, encoding.GetBytes(text + '\0'));
+        public async Task WriteStringAsync(int address, string text, Encoding encoding) => await WriteDataAsync(address, encoding.GetBytes(text + '\0'));
+        #endregion
+
+        #region WriteStringArray
         public void WriteStringArray(IntPtr address, string[] text, Encoding encoding)
         {
-            int totalLength = 0; ;
+            int totalLength = 0;
             foreach (string s in text)
                 totalLength += encoding.GetByteCount(s) + 1;
+
             byte[] buffer = new byte[totalLength];
             int bufferOffset = 0;
+
             foreach (string s in text)
             {
                 int byteCount = encoding.GetBytes(s, buffer.AsSpan(bufferOffset));
-                buffer[bufferOffset + byteCount] = 0;
                 bufferOffset += byteCount + 1;
             }
+
             WriteData(address, buffer);
         }
+        public void WriteStringArray(int address, string[] text, Encoding encoding)
+        {
+            int totalLength = 0;
+            foreach (string s in text)
+                totalLength += encoding.GetByteCount(s) + 1;
+
+            byte[] buffer = new byte[totalLength];
+            int bufferOffset = 0;
+
+            foreach (string s in text)
+            {
+                int byteCount = encoding.GetBytes(s, buffer.AsSpan(bufferOffset));
+                bufferOffset += byteCount + 1;
+            }
+
+            WriteData(address, buffer);
+        }
+        public async Task WriteStringArrayAsync(IntPtr address, string[] text, Encoding encoding)
+        {
+            int totalLength = 0;
+            foreach (string s in text)
+                totalLength += encoding.GetByteCount(s) + 1;
+
+            byte[] buffer = new byte[totalLength];
+            int bufferOffset = 0;
+
+            foreach (string s in text)
+            {
+                int byteCount = encoding.GetBytes(s, buffer.AsSpan(bufferOffset));
+                bufferOffset += byteCount + 1;
+            }
+
+            await WriteDataAsync(address, buffer);
+        }
+        public async Task WriteStringArrayAsync(int address, string[] text, Encoding encoding)
+        {
+            int totalLength = 0;
+            foreach (string s in text)
+                totalLength += encoding.GetByteCount(s) + 1;
+
+            byte[] buffer = new byte[totalLength];
+            int bufferOffset = 0;
+
+            foreach (string s in text)
+            {
+                int byteCount = encoding.GetBytes(s, buffer.AsSpan(bufferOffset));
+                bufferOffset += byteCount + 1;
+            }
+
+            await WriteDataAsync(address, buffer);
+        }
+        #endregion
+
+        #endregion
+
+        #endregion
     }
 }
