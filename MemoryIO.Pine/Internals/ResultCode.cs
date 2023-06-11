167363e2643667233e92a3cbe9df78422d11cd6d
Vi Ness <kevfol@hotmail.com>
2023-06-11 07:44:58 -1000

    Removed Erroneous ResultCodes I don't remember where I found these extra ResultCodes but they are not in the standard documentation or implemented in pcsx2

--- MemoryIO.Pine/Internals/ResultCode.cs
+++ MemoryIO.Pine/Internals/ResultCode.cs
@@ -3,11 +3,6 @@
     internal enum ResultCode : byte
     {
         OK = 0,
-        Fail = 1,
-        OutOfMemory = 2,
-        NoConnection = 3,
-        Unimplemented = 4,
-        Unknown = 5,
         Failure = 0xFF,
     }
 }
